678e89d1ed2b0b6f514d01da2bd3cf80f7dbe4e7
Philip Sampaio <philip.sampaio@gmail.com>
2015-10-16 01:38:07 -0300

    Add the event system to Accounting

--- engines/accounting/lib/accounting.rb
+++ engines/accounting/lib/accounting.rb
@@ -2,8 +2,21 @@ require "accounting/engine"
 
 module Accounting
   class << self
-    def sum(a, b)
-      a + b
+    def event_listeners
+      @event_listeners ||= Hash.new([])
+    end
+
+    # Adds a new event listener.
+    # Listener should be a constant or a string representing a constant,
+    # and that class/module must respond to `.execute`.
+    def add_event_listener(event, listener)
+      event_listeners[event] << listener.to_s
+    end
+
+    def publish_event(event, *args)
+      event_listeners[event].map do |listener|
+        Object.const_get(listener).execute(*args)
+      end
     end
   end
 end
--- engines/accounting/spec/accounting_spec.rb
+++ engines/accounting/spec/accounting_spec.rb
@@ -1,14 +1,50 @@
 require 'spec_helper'
 
 describe Accounting do
+  class DummyListener
+    def self.execute(_data); end
+  end
+
   subject(:accounting) { described_class }
 
-  describe '.sum' do
-    subject(:sum) { accounting.sum(a, b) }
+  let(:listener) { DummyListener }
 
-    let(:a) { 22 }
-    let(:b) { 20 }
+  describe '.add_event_listener' do
+    subject(:add_event_listener) { accounting.add_event_listener(event, listener) }
 
-    it { is_expected.to eq(42) }
+    let(:event) { :transfer_done }
+    before { accounting.event_listeners[event] = [] }
+
+    it {
+      expect { add_event_listener }
+        .to change { accounting.event_listeners[event] }
+        .from([])
+        .to(['DummyListener'])
+    }
+  end
+
+  describe '.publish_event' do
+    subject(:publish_event) { accounting.publish_event(:done_job, 42) }
+
+    context 'when there are listeners' do
+      before do
+        accounting.add_event_listener(:done_job, listener)
+      end
+      after { accounting.event_listeners[:done_job] = [] }
+
+      it 'sends the execution message to the listeners' do
+        expect(listener).to receive(:execute).with(42)
+
+        publish_event
+      end
+    end
+
+    context 'when listeners is not configured' do
+      it 'does not send the execution message to the listener' do
+        expect(listener).to_not receive(:execute)
+
+        publish_event
+      end
+    end
   end
 end
